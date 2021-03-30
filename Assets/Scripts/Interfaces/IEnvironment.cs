ab3a4051c7bd7eb19aed5e4810ce9b350d5bc5ee
kkalera <kkalera@gmail.com>
2021-03-30 17:09:34 +0200

    0.0.2

---
+++ Assets/Scripts/Interfaces/IEnvironment.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IEnvironment
+{
+    float EnvSizeX { get; set; }
+    float EnvSizeY { get; set; }
+    float EnvSizeZ { get; set; }
+}
