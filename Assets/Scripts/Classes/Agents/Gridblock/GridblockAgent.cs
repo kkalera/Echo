4592cd92cb533488800f19b43bbb6aa7df405e77
kkalera <kkalera@gmail.com>
2021-07-11 22:28:34 +0200

    .

--- Assets/Scripts/Classes/Agents/Gridblock/GridblockAgent.cs
+++ Assets/Scripts/Classes/Agents/Gridblock/GridblockAgent.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
+using UnityEngine.InputSystem;
 
 public class GridblockAgent : Agent
 {
+    [SerializeField] private InputAction inputUpDown;
+    [SerializeField] private InputAction inputLeftRight;
+    
     [SerializeField] private Transform _Target;
     [SerializeField] private Transform _Environment;
 
@@ -15,19 +19,18 @@ public class GridblockAgent : Agent
 
     private void Start()
     {
+        inputLeftRight.Enable();
+        inputUpDown.Enable();
         _agentRB = GetComponent<Rigidbody>();
     }
 
     public override void OnEpisodeBegin()
-    {
-        if(transform.localPosition.y < 0)
-        {
-            _agentRB.angularVelocity = Vector3.zero;
-            _agentRB.velocity = Vector3.zero;
-            transform.localPosition = Vector3.zero;
-
-            _Target.localPosition = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
-        }
+    {        
+        _agentRB.angularVelocity = Vector3.zero;
+        _agentRB.velocity = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        transform.localPosition = new Vector3(0,0.5f,0);
+        _Target.localPosition = new Vector3(Random.Range(-5, 5), 1.5f, Random.Range(-5, 5));
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -38,6 +41,15 @@ public class GridblockAgent : Agent
         sensor.AddObservation(_agentRB.velocity.z);
     }
 
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = inputUpDown.ReadValue<float>();
+        continuousActions[1] = inputLeftRight.ReadValue<float>();
+        float rx = Mathf.Min(Mathf.Abs(transform.localPosition.x - _Target.localPosition.x), 1);
+        float rz = Mathf.Min(Mathf.Abs(transform.localPosition.z - _Target.localPosition.z), 1);
+    }
+
     public float forceMultiplier = 10f;
     public override void OnActionReceived(ActionBuffers actions)
     {
@@ -45,21 +57,31 @@ public class GridblockAgent : Agent
         Vector3 controlSignal = Vector3.zero;
         controlSignal.x = actions.ContinuousActions[0];
         controlSignal.z = actions.ContinuousActions[1];
-        _agentRB.AddForce(controlSignal * forceMultiplier);
+
+        transform.localPosition =transform.localPosition + (controlSignal*Time.deltaTime);
+
+        float rx = Mathf.Min(Mathf.Abs(transform.localPosition.x - _Target.localPosition.x),1) / (MaxStep * 4);
+        float rz = Mathf.Min( Mathf.Abs(transform.localPosition.z - _Target.localPosition.z),1) / (MaxStep * 4);
+        //AddReward(rx + rz);
 
         // Rewards
         float distanceToTarget = Vector3.Distance(this.transform.localPosition, _Target.localPosition);
 
         // Reached target
-        if (distanceToTarget < 1.42f)
+        if (distanceToTarget < 2f)
         {
+            Utils.ClearLogConsole();
+            Debug.Log("Win");
             SetReward(1.0f);
+            //AddReward((0.5f /MaxStep)* (MaxStep - StepCount));
             EndEpisode();
         }
 
         // Fell off platform
-        else if (this.transform.localPosition.y < 0)
+        else if (this.transform.localPosition.y < -0.5)
         {
+            Utils.ClearLogConsole();
+            Debug.Log("dead");
             EndEpisode();
         }
     }
