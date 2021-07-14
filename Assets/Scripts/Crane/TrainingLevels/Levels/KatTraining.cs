2959bb1191f19fc5a3579f880bb162bf56170b60
kkalera <kkalera@gmail.com>
2021-07-14 22:52:24 +0200

    .

--- Assets/Scripts/Crane/TrainingLevels/Levels/KatTraining.cs
+++ Assets/Scripts/Crane/TrainingLevels/Levels/KatTraining.cs
@@ -6,6 +6,8 @@ public class KatTraining : CraneLevel
 {
     [SerializeField] private float rewardDistance;
     [SerializeField] private float rewardSpeed;
+    [SerializeField] private float spawnRange;  
+    [SerializeField] private float maxDistance;
     [SerializeField] private GameObject targetPrefab;
     private GameObject target;
 
@@ -23,15 +25,22 @@ public class KatTraining : CraneLevel
 
         if(distance <= rewardDistance && speed <= rewardSpeed)
         {
+            IncreaseDifficulty();
             return new RewardData(1f, true);
         }
+
+        if (distance > maxDistance) return new RewardData(0, true);
+
         return new RewardData();
     }
 
     public override void IncreaseDifficulty()
     {
-        rewardDistance = Mathf.Max(rewardDistance - 0.1f, 0.1f);
-        rewardSpeed = Mathf.Max(rewardSpeed - 0.1f, 0.1f);
+        spawnRange = Mathf.Min(spawnRange + 0.1f, 50f);
+        maxDistance = (spawnRange * 2) + 1;
+
+        if (Mathf.Approximately(spawnRange, 50f)) rewardSpeed = Mathf.Max(rewardSpeed - 0.1f, 0.1f);
+
     }
 
     public override void InitializeEnvironment(Transform environment)
@@ -42,15 +51,21 @@ public class KatTraining : CraneLevel
 
     public override void ResetEnvironment(Transform environment, ICrane crane)
     {
-        target.transform.localPosition = new Vector3(0, 20, Random.Range(-20, 35));        
-        crane.ResetToPosition(new Vector3(0, 20, Random.Range(-20, 35)));
+        float randomZ = Random.Range(-20, 35);
+        float spawnZ = Mathf.Clamp(Random.Range(randomZ - spawnRange, randomZ + spawnRange), -25, 40);
+
+        float randomY = Random.Range(20, 25);
+        float spawnY = Mathf.Clamp(Random.Range(randomY - spawnRange, randomY + spawnRange), -16, 30);
+
+        target.transform.localPosition = new Vector3(0, randomY, randomZ);        
+        crane.ResetToPosition(new Vector3(0, spawnY, spawnZ));
     }
 
     public override ICrane SetCraneRestrictions(ICrane crane)
     {
         crane.CabinMovementDisabled = false;
         crane.CraneMovementDisabled = true;
-        crane.WinchMovementDisabled = true;
+        crane.WinchMovementDisabled = false;
         crane.SwingDisabled = true;
         return crane;
     }
