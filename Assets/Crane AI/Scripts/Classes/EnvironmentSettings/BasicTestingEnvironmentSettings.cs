97134adc6766726e3b7833a8b641f9fdf5e1118e
kkalera <kkalera@gmail.com>
2022-01-09 09:50:16 +0100

    .
    Restarting fresh

--- Assets/Crane AI/Scripts/Classes/EnvironmentSettings/BasicTestingEnvironmentSettings.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BasicTestingEnvironmentSettings : MonoBehaviour
-{
-    public float WallHeight = 0f;
-}
--- Assets/Crane V2/Scripts/V2_CollisionResponse.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class V2_CollisionResponse
-{
-    public bool EndEpisode = false;
-    public float Reward = 0f;
-}
